e4a7c1dc6304fcc4e85c67a17ddc2077b6ba33fd
omodauda <omodauda.dl@gmail.com>
2021-05-26 15:25:15 +0100

    setup module resolver

--- App.js
+++ App.js
@@ -1,30 +1,12 @@
 import React from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import StartUpScreen from '@screens/StartUpScreen';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  // const backgroundStyle = {
-  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  // };
-
   return (
-    <View>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView>
-        <Text>Hello from App</Text>
-      </ScrollView>
-    </View>
+    <>
+      <StartUpScreen />
+    </>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default App;
---
+++ src/screens/StartUpScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function StartUpScreen() {
+  return (
+    <View>
+      <Text>Hello from Start Up Screen</Text>
+    </View>
+  );
+}
