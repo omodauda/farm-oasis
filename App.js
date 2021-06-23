563ac20171b79650844fede0a0488b528bfd8b13
omodauda <omodauda.dl@gmail.com>
2021-06-23 23:21:40 +0100

    create app state store

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import reduxThunk from 'redux-thunk';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from '@navigations/AppNavigator';
 
+const rootReducer = combineReducers({});
+
+const store = createStore(rootReducer, applyMiddleware(reduxThunk));
+
 const App = () => {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
