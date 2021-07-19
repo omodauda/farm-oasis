cc092ded124937f04072b3ad50b9d8998dfdcbb2
omodauda <omodauda.dl@gmail.com>
2021-07-19 13:50:45 +0100

    redux-persist

--- App.js
+++ App.js
@@ -2,23 +2,35 @@ import React from 'react';
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import reduxThunk from 'redux-thunk';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistStore, persistReducer} from 'redux-persist';
+import {PersistGate} from 'redux-persist/integration/react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from '@navigations/AppNavigator';
 
 import authReducer from '@store/reducers/auth';
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['user'],
+};
+
 const rootReducer = combineReducers({
-  auth: authReducer,
+  auth: persistReducer(persistConfig, authReducer),
 });
 
 const store = createStore(rootReducer, applyMiddleware(reduxThunk));
+const persistor = persistStore(store);
 
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 };
